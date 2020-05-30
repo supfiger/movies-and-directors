2a1fe4f11accd2e289f8e6dc79b705d5fc415102
supfiger <danilzin777@gmail.com>
2020-05-30 21:34:53 +0300

    added the abilities to delete movie and director

--- client/src/components/MoviesDialog/MoviesDialog.jsx
+++ client/src/components/MoviesDialog/MoviesDialog.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
-import BlockIcon from '@material-ui/icons/Block';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import BlockIcon from "@material-ui/icons/Block";
+
+import withHocs from "./MoviesDialogHoc";
 
 class MoviesDialog extends React.Component {
-
   handleDelete = () => {
-    const { id, handleClose } = this.props;
+    const { id, handleClose, deleteMovie } = this.props;
+    deleteMovie(id);
     handleClose();
-  }
+  };
 
   render() {
     const { open, handleClose } = this.props;
@@ -25,7 +27,9 @@ class MoviesDialog extends React.Component {
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">{"Are you sire that you want to delete element?"}</DialogTitle>
+        <DialogTitle id="alert-dialog-title">
+          {"Are you sire that you want to delete element?"}
+        </DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
             If you click 'Confirm' this element will be removed from data base.
@@ -36,7 +40,7 @@ class MoviesDialog extends React.Component {
             <BlockIcon /> Cancel
           </Button>
           <Button onClick={this.handleDelete} color="primary" autoFocus>
-            <DeleteForeverIcon/> Confirm
+            <DeleteForeverIcon /> Confirm
           </Button>
         </DialogActions>
       </Dialog>
@@ -44,4 +48,4 @@ class MoviesDialog extends React.Component {
   }
 }
 
-export default MoviesDialog;
+export default withHocs(MoviesDialog);
