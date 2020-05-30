ac827a758e2bdd23c82b306f72ac50b64d172b9a
supfiger <danilzin777@gmail.com>
2020-05-31 00:33:09 +0300

    Updates

---
+++ precache-manifest.59fc5acf89bff702d10b958a3e79dcbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d0462fcfaf3c4adbf045267f3509783",
+    "url": "/apps/movies-and-directors/index.html"
+  },
+  {
+    "revision": "4bec5643a09b05f7fc65",
+    "url": "/apps/movies-and-directors/static/css/main.0466dad6.chunk.css"
+  },
+  {
+    "revision": "bd9ca794c1f08de6ac3e",
+    "url": "/apps/movies-and-directors/static/js/2.8a0d21e3.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/apps/movies-and-directors/static/js/2.8a0d21e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bec5643a09b05f7fc65",
+    "url": "/apps/movies-and-directors/static/js/main.38ef9e5f.chunk.js"
+  },
+  {
+    "revision": "c7476bf23388682f8638",
+    "url": "/apps/movies-and-directors/static/js/runtime-main.b85e161d.js"
+  }
+]);
